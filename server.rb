714b9dd122f0d102460a8281117caa9ea6a6cf93
Juan Brizuela <juan.brizuela@michelada.io>
2019-08-16 14:42:37 -0500

    Server now responds with static_files

--- server.rb
+++ server.rb
@@ -47,6 +47,7 @@ class Response
     @status = status
     @headers = DEFAULT_HEADERS.merge(headers)
     @headers["Content-Length"] = response.bytesize
+    @headers["Date"] = DateTime.now.new_offset(0).strftime("%a, %e %b %Y %k:%M:%S GMT")
     @response = response
   end
 
@@ -60,13 +61,81 @@ class Response
   end
 end
 
+class StaticResource
+  attr_reader :base_path, :default_document, :files
 
+  MIME_TYPES = {
+    "hmtl": "text/html",
+    "css": "text/css",
+    "gif": "image/gif",
+    "jpg": "image.jpg",
+    "jpeg": "image/jpeg",
+    "js": "text/javascript",
+    "json": "application/json"
+  }.freeze
+
+  BINARY_TYPES = ["gif", "jpg", "jpeg", "png"].freeze
+
+  def initialize(base_path: , default_document:)
+    @base_path = base_path
+    @default_document = default_document
+    @files = []
+  end
+
+  def exists?(resource)
+    path = resource_path(resource)
+    return path true if @files.include?(path)
+
+    if File.exists?(path)
+      @files.push(path)
+      return true
+    end
+
+    false
+  end
+
+  def load(resource)
+    path = resource_path(resource)
+    extension = File.extname(path).sub(".", "").downcase
+    
+    if BINARY_TYPES.include?(extension)
+      File.binread(path)
+    else
+      File.read(path)
+    end
+  end
+  
+  def mime_type(resource)
+    path = resource_path(resource)
+    extension = File.extname(path).sub(".", "").downcase
+
+    MIME_TYPES[:"#{extension}"] || "text/#{extension}"
+  end
+
+  private
+
+  def resource_path(resource)
+    clean_resource = resource.gsub("../", "").split("?")[0]
+    clean_resource = default_document if clean_resource == "/"
+
+    File.join(base_path, clean_resource)
+  end
+end
+
+
+
+
+ROOT = __dir__
+DEFAULT_DOCUMENT = "index.html"
+PUBLIC_PATH = File.join(ROOT, "public")
 
 host = ENV["BIND_ADDRESS"] || "localhost"
 port = ENV["PORT"] ? ENV["PORT"].to_i : 2345
 
 server = TCPServer.new(host, port)
 STDERR.puts "Starting server at #{host}:#{port}"
+
+static_resource = StaticResource.new(base_path: PUBLIC_PATH, default_document: DEFAULT_DOCUMENT)
 
 loop do
   session = server.accept
@@ -78,9 +147,18 @@ loop do
 
   STDERR.puts "------------------"
 
-  data = "Hola desde Ruby"
-  
-  response = Response.new(response: data)
+  data = "Not found"
+  status = 404
+  headers = { "Server": "Ruby Server" }
+
+  if static_resource.exists?(request.path)
+    status = 200
+    data = static_resource.load(request.path)
+    content_type = static_resource.mime_type(request.path)
+    headers[:"Content-Type"] = content_type
+  end
+
+  response = Response.new(status: status, headers: headers, response: data)
   session.print(response.render)
 
   session.close
